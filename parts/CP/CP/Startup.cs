790a1231f944f92377015bcda88ad5645028e406
wowarjuna <wowarjuna@gmail.com>
2014-10-29 17:05:52 +0530

    new project and template updates

---
+++ parts/CP/CP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CP.Startup))]
+namespace CP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
