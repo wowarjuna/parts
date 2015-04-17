9f3a5964b4a8aba9f30b73d0154a80d983dca0a2
wowarjuna <wowarjuna@gmail.com>
2015-04-17 17:19:44 +0530

    reports changes

---
+++ parts/CP/CP/Areas/Store/Controllers/ReportController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CP.Areas.Store.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Store/Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
